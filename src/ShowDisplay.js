630e82789d5e7103fce15045bc38add161163ec3
LidesSam <lides.sam@gmail.com>
2021-10-25 00:26:06 -0300

    v0.1.1 file load json

--- src/ShowDisplay.js
+++ src/ShowDisplay.js
@@ -12,22 +12,54 @@ class ShowcaseDisplay {
 
         this.boxStyle = "boxSizing: border-box, color:black , borderColor:black"
         this.showboxes = []
+        this.addFromJsonFile = this.addFromJsonFile.bind(this)
     }
 
+    /**
+     * 
+     * @param {*} path 
+     */
+    addFromJsonFile(path) {
+
+        fetch(path)
+            .then(response => response.json())
+            .then(data => {
+                for (var i=0;i<=data.length-1;i++){
+                    let box= data[i]
+                    console.log(i)
+                    console.log(data[i])
+                    console.log(box)
+                    console.log(box.backcolor)
+                    switch(box.type){
+                        case"showbox":
+                            this.createShowBox(box.title,box.gitLink,box.deployLink,box.descrip,box.backcolor)
+                            break;
+                        default:
+                            let sep1 = document.createElement("br")
+                            let sep2 = document.createElement("hr")
+                            this.parent.appendChild(sep1)
+                            this.parent.appendChild(sep2)
+                            break;
+                    }
+                    //console.log(box.title)
+                
+                }
+            })
+    }
 
     createShowBox(title = "", gitHubUrl = null, playUrl = null, _description = "", backcolor = "#ff9822") {
         console.log("addd sghow box")
         let showbox = document.createElement("div")
         let sb_title = document.createElement("h2")
         sb_title.textContent = title
-
-
+        console.log(backcolor)
         //style of the showbox
         showbox.style.background = backcolor
         showbox.style.minHeight = "64px"
         showbox.style.minWidth = "128px"
         showbox.style.border = "black 2px solid"
         showbox.style.boxSizing = "border-box"
+        showbox.style.minHeight="fit-content"
 
 
 
@@ -35,7 +67,7 @@ class ShowcaseDisplay {
         let gitLink = document.createElement("a")
         let playLink = document.createElement("a")
         let linkbar = document.createElement("div")
-
+        linkbar.setAttribute("class","hbox")
         linkbar.appendChild(gitLink)
         linkbar.appendChild(playLink)
 
@@ -64,9 +96,9 @@ class ShowcaseDisplay {
         }//
 
 
-        linkbar.style.padding="4px";
+        linkbar.style.padding = "4px";
         showbox.appendChild(linkbar)
-        
+
 
 
         this.parent.appendChild(showbox)
